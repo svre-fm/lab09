22271a819bba39d99f7c7fee6412045a8d443c42
svre-fm <firmoioi96@gmail.com>
2024-12-13 15:35:38 +0700

    complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,14 +2,24 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+double interest(double money,double rate){
+	return money * (rate/100);
+}
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+double payment(double pay,double total){
+	if(total >= pay) return pay;
+	else return total; 
+}
+
+int main(){	
+	double money,rate,pay;
+	cout << "Enter initial loan: ";
+	cin >> money;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +28,25 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int i =  1;
+	double total,New;
+	do{
+		cout << fixed << setprecision(2); 
+
+		total = money+interest(money,rate);
+		New = total-payment(pay,total);
+
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << money;
+		cout << setw(13) << left << interest(money,rate);
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << payment(pay,total);
+		cout << setw(13) << left << New;
+		cout << "\n";	
+
+		money = New;
+		i++;
+	}while(New != 0);
 	
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int n,int m){
+	if(n <= 0 or m <= 0) cout << "Invalid input";
+	else{ 
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < m; j++){
+				cout << "O";
+			}
+		cout << "\n";
+		}
+	}
+}
 
 int main(){
 	
